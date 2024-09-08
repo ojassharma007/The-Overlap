f3f0a37d48342c940606c91e841c75fa5ebfa0fb
Ojas Sharma <115308923+ojas2708@users.noreply.github.com>
2024-09-08 16:10:16 +0530

    changes for dep

--- Football/settings.py
+++ Football/settings.py
@@ -80,12 +80,7 @@ WSGI_APPLICATION = "Football.wsgi.app"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
+DATABASES = {}
 
 
 # Password validation

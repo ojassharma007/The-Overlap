cb3ec6fe3b6b73d9074cce49f851b141b3d38fd3
Ojas Sharma <115308923+ojas2708@users.noreply.github.com>
2024-11-21 04:27:35 +0530

    add white noise

--- Football/settings.py
+++ Football/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    'whitenoise.middleware.WhiteNoiseMiddleware',  #added
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -125,6 +126,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles", "static")
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
